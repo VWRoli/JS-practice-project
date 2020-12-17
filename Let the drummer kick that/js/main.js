fb13e3154aa122a868891641677a7a18dbedc75d
Roland Füst <31432331+VWRoli@users.noreply.github.com>
2020-12-17 19:59:20 +0100

    Add files via upload

--- Let the drummer kick that/js/main.js
+++ Let the drummer kick that/js/main.js
@@ -24,38 +24,47 @@ function addDrums() {
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'a') {
+    document.getElementById('clap-sound').currentTime = 0;
     document.getElementById('clap-sound').play();
     document.getElementById('clap').classList.add('play-drums');
     removeDrums();
   } else if (e.key === 's') {
+    document.getElementById('hi-hat-sound').currentTime = 0;
     document.getElementById('hi-hat-sound').play();
     document.getElementById('hi-hat').classList.add('play-drums');
     removeDrums();
   } else if (e.key === 'd') {
+    document.getElementById('kick-sound').currentTime = 0;
     document.getElementById('kick-sound').play();
     document.getElementById('bass').classList.add('play-drums');
     removeDrums();
   } else if (e.key === 'f') {
+    document.getElementById('open-hat-sound').currentTime = 0;
     document.getElementById('open-hat-sound').play();
     document.getElementById('open-hat').classList.add('play-drums');
     removeDrums();
   } else if (e.key === 'g') {
+    document.getElementById('boom-sound').currentTime = 0;
     document.getElementById('boom-sound').play();
     document.getElementById('boom').classList.add('play-drums');
     removeDrums();
   } else if (e.key === 'h') {
+    document.getElementById('ride-sound').currentTime = 0;
     document.getElementById('ride-sound').play();
     document.getElementById('ride').classList.add('play-drums');
     removeDrums();
   } else if (e.key === 'j') {
+    document.getElementById('snare-sound').currentTime = 0;
     document.getElementById('snare-sound').play();
     document.getElementById('snare').classList.add('play-drums');
     removeDrums();
   } else if (e.key === 'k') {
+    document.getElementById('tom-sound').currentTime = 0;
     document.getElementById('tom-sound').play();
     document.getElementById('tom').classList.add('play-drums');
     removeDrums();
   } else if (e.key === 'l') {
+    document.getElementById('tink-sound').currentTime = 0;
     document.getElementById('tink-sound').play();
     document.getElementById('tink').classList.add('play-drums');
     removeDrums();
