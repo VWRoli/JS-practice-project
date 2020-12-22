ba261bd6c8ae258c22013a1ee806b65dbd089fdb
VWRoli <fustroli@gmail.com>
2020-12-22 17:51:29 +0100

    added some JS functionality

--- Calculator/js/main.js
+++ Calculator/js/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const btnEL = document.querySelectorAll('.btn');
+const delEl = document.querySelector('.btn-delete');
+const smallDisplayEL = document.querySelector('.small-display');
+const mainDisplayEl = document.querySelector('.main-display');
+
+let userInput = '';
+
+//User input to a string
+
+for (let i = 0; i < btnEL.length; i++)
+  btnEL[i].addEventListener('click', function () {
+    userInput += btnEL[i].textContent;
+    //display user input
+    smallDisplayEL.textContent = userInput;
+    console.log(userInput);
+  });
+
+//Results
+document.querySelector('.btn-equal').addEventListener('click', function () {
+  const result = math.evaluate(userInput);
+  mainDisplayEl.textContent = result;
+});
+
+//Clear button
+document.querySelector('.btn-clear').addEventListener('click', function () {
+  userInput = '';
+  console.log(userInput);
+});
+
+//Backspace button
+delEl.addEventListener('click', function () {
+  if (userInput.length > 0) {
+    let deleteLastCh = userInput.slice(0, -1);
+    userInput = deleteLastCh;
+    console.log(userInput);
+  }
+});
