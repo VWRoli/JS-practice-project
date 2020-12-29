83e4ec6b52e0eef699e64e28dd5fdccf19edcf92
VWRoli <fustroli@gmail.com>
2020-12-29 11:24:53 +0100

    completed color flipper

--- color flipper/main.js
+++ color flipper/main.js
@@ -1,19 +1,20 @@
 'use strict';
 
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-const randomNumbers = [];
-const randomHex = [];
 
-//generating a six digit ranfom number
-for (let i = 0; i < 6; i++) {
-  let rng = Math.floor(Math.random() * hex.length);
-  randomNumbers.push(rng);
-}
-console.log(randomNumbers);
-//Generating a random hex number
-const randomHexGenerator = randomNumbers.map((i) => (i = hex[i])).join('');
-console.log(randomHexGenerator);
-
+// Click event
 document.querySelector('#random-color').addEventListener('click', function () {
-  console.log('clicked');
+  const randomNumbers = [];
+  //generating a six digit random number
+  for (let i = 0; i < 6; i++) {
+    let rng = Math.floor(Math.random() * hex.length);
+    randomNumbers.push(rng);
+  }
+  //Generating a random hex number
+  const randomHexGenerator = randomNumbers.map((i) => (i = hex[i])).join('');
+
+  //change body background to hex number
+  document.body.style.backgroundColor = `#${randomHexGenerator}`;
+  //Change text to hex number
+  document.querySelector('#color-code').textContent = `#${randomHexGenerator}`;
 });
