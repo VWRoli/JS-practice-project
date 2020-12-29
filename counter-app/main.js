296a0dc30d9e64850c83e16574e6f98d04f0c9ef
VWRoli <fustroli@gmail.com>
2020-12-29 15:08:12 +0100

    counter app completed

--- counter-app/main.js
+++ counter-app/main.js
@@ -1,1 +1,41 @@
 'use strict';
+const counterDisplayEl = document.querySelector('.counter-display');
+//Counter
+let counter = 0;
+
+const setCounter = function () {
+  counterDisplayEl.textContent = counter;
+};
+setCounter();
+
+//Colorization
+const colorization = function () {
+  if (counter > 0) {
+    counterDisplayEl.classList.remove('negative');
+    counterDisplayEl.classList.add('positive');
+  } else if (counter < 0) {
+    counterDisplayEl.classList.remove('positive');
+    counterDisplayEl.classList.add('negative');
+  } else {
+    counterDisplayEl.classList.remove('positive');
+    counterDisplayEl.classList.remove('negative');
+  }
+};
+//decrease button
+document.querySelector('#decrease').addEventListener('click', function () {
+  counter--;
+  setCounter();
+  colorization();
+});
+//reset button
+document.querySelector('#reset').addEventListener('click', function () {
+  counter = 0;
+  setCounter();
+  colorization();
+});
+//increase button
+document.querySelector('#increase').addEventListener('click', function () {
+  counter++;
+  setCounter();
+  colorization();
+});
