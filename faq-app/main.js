804ce7f899307ce9127b505febc580ced856c0a2
VWRoli <fustroli@gmail.com>
2021-01-02 14:44:54 +0100

    faq completed

--- faq-app/main.js
+++ faq-app/main.js
@@ -1,1 +1,8 @@
 'use strict';
+const questionElAll = document.querySelectorAll('.fa-plus-square');
+
+for (let i = 0; i < questionElAll.length; i++) {
+  questionElAll[i].addEventListener('click', function () {
+    this.nextElementSibling.classList.toggle('show-text');
+  });
+}
