912191c2375e035981446f6ee68a8f2265a99351
VWRoli <fustroli@gmail.com>
2021-01-06 11:05:04 +0100

    Added flip card functionality

--- memory-card-game/main.js
+++ memory-card-game/main.js
@@ -1,1 +1,11 @@
 'use strict';
+//Variables
+const cardEl = document.querySelectorAll('.card-items');
+const cardWrapperEl = document.querySelectorAll('.card-wrapper');
+
+//Flip cards
+for (let i = 0; i < cardEl.length; i++)
+  cardEl[i].addEventListener('click', function () {
+    console.log('click');
+    cardWrapperEl[i].classList.toggle('active');
+  });
