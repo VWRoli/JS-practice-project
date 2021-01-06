221df60cc6e955858d7302314f6e88be9533dcc3
VWRoli <fustroli@gmail.com>
2021-01-06 11:48:32 +0100

    finished css

--- memory-card-game/main.js
+++ memory-card-game/main.js
@@ -2,6 +2,9 @@
 //Variables
 const cardEl = document.querySelectorAll('.card-items');
 const cardWrapperEl = document.querySelectorAll('.card-wrapper');
+const startBtnEl = document.querySelector('.start-btn');
+const showcaseEl = document.querySelector('.showcase');
+const gameEl = document.querySelector('.game');
 
 //Flip cards
 for (let i = 0; i < cardEl.length; i++)
@@ -9,3 +12,10 @@ for (let i = 0; i < cardEl.length; i++)
     console.log('click');
     cardWrapperEl[i].classList.toggle('active');
   });
+
+//Start game
+startBtnEl.addEventListener('click', function () {
+  console.log('click');
+  showcaseEl.classList.toggle('hidden');
+  gameEl.classList.toggle('hidden');
+});
