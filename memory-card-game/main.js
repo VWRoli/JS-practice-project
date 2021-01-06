a8652251f3860869c553292910b55b809f7efe28
VWRoli <fustroli@gmail.com>
2021-01-06 14:42:09 +0100

    added timing functionality

--- memory-card-game/main.js
+++ memory-card-game/main.js
@@ -5,6 +5,11 @@ const cardWrapperEl = document.querySelectorAll('.card-wrapper');
 const startBtnEl = document.querySelector('.start-btn');
 const showcaseEl = document.querySelector('.showcase');
 const gameEl = document.querySelector('.game');
+const timerEl = document.querySelector('.timer');
+const resetBtnEl = document.querySelector('.btn-reset');
+
+let time = 0;
+let timer;
 
 //Flip cards
 for (let i = 0; i < cardEl.length; i++)
@@ -15,7 +20,32 @@ for (let i = 0; i < cardEl.length; i++)
 
 //Start game
 startBtnEl.addEventListener('click', function () {
-  console.log('click');
+  startTimer(time);
   showcaseEl.classList.toggle('hidden');
   gameEl.classList.toggle('hidden');
+});
+
+//Start counter
+const startTimer = (time) => {
+  //Start counting seconds
+  timer = setInterval(function () {
+    const now = new Date();
+    time++;
+    //Convert time to minutes and seconds
+    let min = String(Math.trunc(time / 60)).padStart(2, 0);
+    let sec = String(time % 60).padStart(2, 0);
+    //Show time
+    timerEl.textContent = `${min}:${sec}`;
+  }, 1000);
+};
+
+//Reset button
+resetBtnEl.addEventListener('click', function () {
+  //Reset timer
+  clearInterval(timer);
+  time = 0;
+  //Show time
+  timerEl.textContent = `00:00`;
+  //Start timer again
+  startTimer(time);
 });
