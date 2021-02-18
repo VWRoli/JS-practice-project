db58f7ffca75782baf37fedd276a878f3d2bddc1
VWRoli <fustroli@gmail.com>
2021-02-18 11:32:43 +0100

    created board

--- chess/js/mains.js
+++ chess/js/mains.js
@@ -1,5 +1,26 @@
 import { Chess } from 'chess.js';
 const chess = new Chess();
+
 const board = chess.board();
+
 console.log(chess);
 console.log(board);
+
+const fen = chess.fen();
+console.log(fen);
+
+//Creating empty board
+const squares = chess.SQUARES;
+
+const boardElement = document.querySelector('#board');
+
+squares.forEach((el) => {
+  const square = document.createElement('div');
+  square.className = 'square';
+  square.id = el;
+  boardElement.appendChild(square);
+});
+
+//Set square text ID's
+const squareElements = document.querySelectorAll('.square');
+console.log(squareElements);
---
+++ chess/readme.md
@@ -0,0 +1,3 @@
+# Multiplayer Chess Game
+
+Working on building a multiplayer chess game with the help of the chess.js library
